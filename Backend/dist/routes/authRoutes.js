5a5dda86fe77bee3cc24580086cefa7fa7a3a522
Rajat <rajatgsharma29@gmail.com>
2025-05-06 20:35:15 +0530

    Remove dist folder from repository

--- Backend/dist/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import authController from "../controller/authController.js";
-import validateHrData from "../public/validatingHrData.js";
-const router = Router();
-router.post('/login', validateHrData.validateLoginData, authController.postLogin);
-router.post('/signup', validateHrData.validateSignupData, authController.postSignup);
-router.get('/logout', authController.logout);
-export default router;
